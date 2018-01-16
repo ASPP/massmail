47e904db657de8bc0a8139297816adccb5b4ad9e
ASPP Student <student@localhost>
2018-01-16 15:54:12 +1100

    remove test for local branch from branch

--- test_localmax.py
+++ test_localmax.py
@@ -1,18 +1,10 @@
-import numpy
-
 def test_len_list():
     nlist = [1, 4, 5]
     assert len(nlist) == 5
 
 def test_float_isseu():
-    assert round(1.2 + 2.2,1) == 3.4
-
-def test_numpy_array_add():
-    x= numpy.array([1,1])
-    y= numpy.array([2,2])
-    z=numpy.array([3,3])
-    assert (x+y==z).all()
+    assert 1.2 + 2.2 == 3.4
 
 
 def test_integer_sum():
-    assert 1 + 3 == 4
+    assert 1 + 3 == 3
