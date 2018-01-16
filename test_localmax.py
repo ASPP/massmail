012c702116ca9a240bd1902e4722da7cb223cbb7
ASPP Student <student@localhost>
2018-01-16 15:33:39 +1100

    something

--- test_localmax.py
+++ test_localmax.py
@@ -1,10 +1,18 @@
+import numpy
+
 def test_len_list():
     nlist = [1, 4, 5]
     assert len(nlist) == 5
 
 def test_float_isseu():
-    assert 1.2 + 2.2 == 3.4
+    assert round(1.2 + 2.2,1) == 3.4
+
+def test_numpy_array_add():
+    x= numpy.array([1,1])
+    y= numpy.array([2,2])
+    z=numpy.array([3,3])
+    assert (x+y==z).all()
 
 
 def test_integer_sum():
-    assert 1 + 3 == 3
+    assert 1 + 3 == 4
