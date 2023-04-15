99ec1f0a8160012c972534bd5356e261194cf366
Tiziano Zito <opossumnano@gmail.com>
2023-04-15 19:08:31 +0200

    properly handle SMTP server binary output in tests

--- massmail/test_massmail.py
+++ massmail/test_massmail.py
@@ -61,28 +61,30 @@ def parse_smtp(server):
     # we can not just issue a blank .read() because that would would block until
     # server.stderr is closed, which only happens after the server has exited
     # so we request 1MB (1024*1024 = 2^20 bytes = 1048576) to be sure
-    smtp = server.stderr.read(1048576).decode()
+    smtp = server.stderr.read(1048576)
     protocol = []
     emails = []
     for line in smtp.splitlines():
-        if line.startswith('INFO:mail.log:'):
+        if line.startswith(b'INFO:mail.log:'):
             # this is a line of protocol: SMTP server <-> SMTP client
             protocol.append(line)
-        elif line.startswith('---------- MESSAGE FOLLOWS ----------'):
+        elif line.startswith(b'---------- MESSAGE FOLLOWS ----------'):
             # this lines starts a new email message
             email = []
-        elif line.startswith('------------ END MESSAGE ------------'):
+        elif line.startswith(b'------------ END MESSAGE ------------'):
             # this line closes an email message
             # join all collected lines
-            email = '\n'.join(email)
-            # parse the whole thing as a string to get a Message object from
-            # the email module
-            emails.append(email_module.message_from_string(email,
+            email = b'\n'.join(email) # we are dealing with bytes here
+            # parse the whole thing to get a EmailMessage object from the email module
+            emails.append(email_module.message_from_bytes(email,
                                             policy=email_module.policy.default))
         else:
             # collect this line, it belongs to the current email message
             email.append(line)
-    protocol = '\n'.join(protocol)
+    protocol = b'\n'.join(protocol)
+    # protocol chat should always be ascii, if we get a UnicodeDecodeError here
+    # we'll have to examine the server output to understand what's up
+    protocol = protocol.decode('ascii')
     return protocol, emails
 
 # wrapper for running the massmail script and parse the SMTP server output
@@ -137,8 +139,8 @@ def test_regular_sending(server, parm, body):
     assert email.get_charsets() == ['utf-8']
     assert email.get_content_type() == 'text/plain'
 
-    # check that we have insert the right values for our keys
-    text = email.get_payload()
+    # check that we have inserted the right values for our keys
+    text = email.get_content()
     assert 'Dear Alice Joyce' in text
     assert 'we kindly invite you to join us in the jungle' in text
 
@@ -151,7 +153,7 @@ def test_unicode_body_sending(server, parm, body):
     email = emails[0]
     # unicode characters force the transfer encoding to  8bit
     assert email['Content-Transfer-Encoding'] == '8bit'
-    text = email.get_payload()
+    text = email.get_content()
     assert 'Üni©ödę¿' in text
 
 def test_wild_unicode_body_sending(server, parm, body):
@@ -163,19 +165,19 @@ def test_wild_unicode_body_sending(server, parm, body):
     # because we use unicode characters that don't fit in one byte,
     # the email will be encoded in base64 for tranfer
     assert email['Content-Transfer-Encoding'] == 'base64'
-    # because the text contains, we trust the internal email_module machinery
+    # we have to trust the internal email_module machinery
     # to perform the proper decoding
     text = email.get_content()
     assert 'œ´®†¥¨ˆøπ¬˚∆˙©ƒ∂ßåΩ≈ç√∫˜µ≤ユーザーコードa😀' in text
 
 def test_unicode_subject(server, parm, body):
-    opts = ['--subject', 'Üni©ödę¿']
+    opts = {'--subject' : 'Üni©ödę¿' }
     protocol, emails = cli(server, parm, body, opts=opts)
     email = emails[0]
     assert email['Subject'] == 'Üni©ödę¿'
 
 def test_unicode_from(server, parm, body):
-    opts = ['--from', 'Üni©ödę¿ <broken@email.com>']
+    opts = { '--from' : 'Üni©ödę¿ <broken@email.com>' }
     protocol, emails = cli(server, parm, body, opts=opts)
     email = emails[0]
     assert email['From'] == 'Üni©ödę¿ <broken@email.com>'
@@ -193,12 +195,12 @@ def test_unicode_several_reciepients(server, parm, body):
 
     assert emails[0]['To'] == 'donkeys@jungle.com'
     assert emails[1]['To'] == 'j@monkeys.com'
-    assert 'Dear Alice Joyce' in emails[0].get_payload()
-    assert 'Dear John Smith' in emails[1].get_payload()
+    assert 'Dear Alice Joyce' in emails[0].get_content()
+    assert 'Dear John Smith' in emails[1].get_content()
 
 def test_unicode_parm(server, parm, body):
     # add some unicode text to the body
     with parm.open('at') as parmf:
         parmf.write('\nÜni©ödę¿; Smith; j@monkeys.com\n')
     protocol, emails = cli(server, parm, body)
-    assert 'Dear Üni©ödę¿ Smith' in emails[1].get_payload()
+    assert 'Dear Üni©ödę¿ Smith' in emails[1].get_content()
